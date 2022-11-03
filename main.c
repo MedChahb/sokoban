cb98e715bbaf0d584ee9ab32c7f5a869c18d7301
CHAHBAOUI MOHAMMED <mohammed.chahbaoui@etu.unistra.fr>
2022-11-03 21:49:26 +0100

    testing the lastest changes

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 int main(void){
 	Grid grille; 
 	init_level("/home/chahbaoui/Desktop/projet_tech_dev/sokoban/level1.txt", &grille); 
-
 	display(&grille);
+	
 	/*	
 	for(int i=0; i<286; i++){
 		printf("%c",grille.game_grid[i]);
