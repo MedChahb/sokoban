c208978cfdd7ff1655ce16c12c8fccdbc5409513
CHAHBAOUI MOHAMMED <mohammed.chahbaoui@etu.unistra.fr>
2022-11-04 00:58:11 +0100

    controle the Player via keyboard

--- main.c
+++ main.c
@@ -6,23 +6,30 @@ int main(void){
 	init_level("/home/chahbaoui/Desktop/projet_tech_dev/sokoban/level1.txt", &grille); 
 	display(&grille);
 	
-	/*	
-	for(int i=0; i<286; i++){
-		printf("%c",grille.game_grid[i]);
-	}
-	*/
-	/*
 	bool run = true;
 	while(run){
 		char entry = fgetc(stdin);
 		switch(entry){
-			case 'q' :{
+			case 'z':
+				move_player(&grille, TOP);
+				break;
+			case 's':
+				move_player(&grille, BOTTOM);
+				break;
+			case 'q':
+				move_player(&grille, LEFT);
+				break;
+			case 'd':
+				move_player(&grille, RIGHT);
+				break;
+			
+			case 'x' :{
 				run = false;
 				break;
 			}
 		}
 	}
-	*/
+	
 	
 }
 
