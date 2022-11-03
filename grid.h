dc3ac7cb2c12da1ccaf38981e217bddf6dfb0c74
CHAHBAOUI MOHAMMED <mohammed.chahbaoui@etu.unistra.fr>
2022-11-03 20:36:49 +0100

    made some changes in struct

--- grid.h
+++ grid.h
@@ -12,11 +12,11 @@ enum CaseType{
  * @brief Cette structure contient les informations 
  * concernant la grille du jeu et son contenu
  */
-struct Grid{
-	enum CaseType** game_grid; ///< Tableau contenant les entités présents dans le jeu
+typedef struct {
+	enum CaseType* game_grid; ///< Tableau contenant les entités présents dans le jeu
 	int column_number; ///< Nombre de colonne de game_grid
 	int row_number; ///< Nomber de ligne de game_grid
-};
-void init_level(const char* file_path);
+}Grid ;
+void init_level(const char* file_path, Grid* grille);
 
 #endif
