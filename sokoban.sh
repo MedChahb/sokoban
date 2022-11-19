c7bbfa940c09c6006fbe872b0259ded107c72e96
CHAHBAOUI MOHAMMED <mohammed.chahbaoui@etu.unistra.fr>
2022-11-19 15:16:17 +0100

    upgraded script

--- sokoban.sh
+++ sokoban.sh
@@ -1,11 +1,11 @@
-sudo apt-get install doxygen
-sudo apt-get install graphviz
 make clean
 make
 echo ""
 echo "TYPE ./bin/main TO PLAY SOKOBAN"
 echo ""
 
+# ouverture de la documentation Doxygen 
 firefox doc/html/index.html
 
+#termination
 kill 0
