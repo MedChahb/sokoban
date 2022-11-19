2f92e503e7f3742f1adc704690b662aa5735e0fc
CHAHBAOUI MOHAMMED <mohammed.chahbaoui@etu.unistra.fr>
2022-11-19 15:38:58 +0000

    remove firefox pop op

--- sokoban.sh
+++ sokoban.sh
@@ -4,8 +4,8 @@ echo ""
 echo "TYPE ./bin/main TO PLAY SOKOBAN"
 echo ""
 
-# ouverture de la documentation Doxygen 
-firefox doc/html/index.html
+# ouverture de la documentation Doxygen (annuler puisque on a le lien ^^) 
+#firefox doc/html/index.html
 
 #termination
 kill 0
