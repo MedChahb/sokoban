ada6c87d4a5bc715419481a0583dc3edd758684f
mohammed chahbaoui <mohammed.chahbaoui@etu.unistra.fr>
2022-12-05 09:55:19 +0100

    changer struct grid

--- include/grid.h
+++ include/grid.h
@@ -30,6 +30,8 @@ enum CaseType{
 typedef struct {
 	enum CaseType* game_grid; /**< Tableau contenant les entités présents dans le jeu */
 	int column_number; /**< Nombre de colonne de game_grid */
+	int nbr_cibles; /**< Nombre de cibles avant le commencement du jeu*/
+	int nbr_cibles_couvert; /**< Nombre de cibles convertes */
 	int row_number; /**< Nomber de ligne de game_grid */
 	struct player Player; /**< Le joueur avec ces coordonnées */
 }Grid ;
@@ -39,5 +41,6 @@ typedef struct {
 void init_level(const char* file_path, Grid* grille);
 void display(Grid* grille);
 void move_player(Grid* grille, enum Direction dir);
+void coord_goals(Grid* grille);
 
 #endif
